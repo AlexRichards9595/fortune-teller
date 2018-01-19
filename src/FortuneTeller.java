a79102213b0117a09c4fceb8ce1c20d6502b452e
Alex <wcadidas15@gmail.com>
2018-01-19 11:28:53 -0500

    Bank account done

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -6,6 +6,7 @@ public class FortuneTeller {
 		
 		Scanner input = new Scanner(System.in);
 		
+		//Getting all the information
 		System.out.println("Please enter your first name:");
 		String firstName = input.next();
 		System.out.println("Please enter your last name:");
@@ -25,13 +26,65 @@ public class FortuneTeller {
 		System.out.println("Please enter the number of siblings that you have:");
 		int siblings = input.nextInt();
 		
+		//Setting years until retirement
 		int retirementYears;
 		if (age%2 == 0) {
 			retirementYears = 3;
 		} else { 
 			retirementYears = 37;
 		}
-		System.out.println(retirementYears);
+		
+		
+		// Where you will retire
+		String vacationHome;
+		if (siblings == 0) {
+			vacationHome = ("Johannesburg, South Africa");
+		} else if (siblings == 1) {
+			vacationHome = ("Moscow, Russia");
+		} else if (siblings == 2) {
+			vacationHome = ("Cabo, Mexico");
+		} else if (siblings == 3) {
+			vacationHome = ("Seattle, Washington");
+		} else if (siblings > 3) {
+			vacationHome = ("Tampa Florida");
+		} else {
+			vacationHome = "Columbus, Ohio haha";
+		}
+		
+		// What you will drive 
+		String transportation;
+		if (color.equalsIgnoreCase("red")) {
+			transportation = ("a red Ferrari");
+		} else  if (color.equalsIgnoreCase("orange")) {
+			transportation = ("a VW bus");
+		} else if (color.equalsIgnoreCase("yellow")) {
+			transportation = ("a Ducati");
+		} else if (color.equalsIgnoreCase("green")) {
+			transportation = ("some gas guzzler");
+		} else if (color.equalsIgnoreCase("blue")) {
+			transportation = ("a prius");
+		} else if (color.equalsIgnoreCase("indigo")) {
+			transportation = ("an audi R8");
+		} else if (color.equalsIgnoreCase("violet")) {
+			transportation = ("a hot air balloon");
+		} else {
+			transportation = ("Your mom's old bike");
+		}
+		
+		// Your bank account balance
+		int bankAccount;
+		if (birthMonth > 0 && birthMonth <= 4 ) {
+			bankAccount = 95;
+		} else if (birthMonth > 4 && birthMonth <= 8 ) {
+			bankAccount = 584736;
+		} else if (birthMonth > 8 && birthMonth <= 12 ) {
+			bankAccount = 1000000004;
+		} else {
+			bankAccount = 0;
+		}
+		
+		
+		
 	}
 
 }
