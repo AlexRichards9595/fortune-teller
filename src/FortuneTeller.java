9b1e8b70fa346338fb09feef83142b39d38768df
Alex <wcadidas15@gmail.com>
2018-01-19 10:34:00 -0500

    ROYGBIV done

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
 public class FortuneTeller {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter your first name:");
+		String firstName = input.next();
+		System.out.println("Please enter your last name:");
+		String lastName = input.next();
+		System.out.println("Please enter your age:");
+		int age = input.nextInt();
+		System.out.println("Please enter your birth month:");
+		int birthMonth = input.nextInt();
+		System.out.println("Please enter your favorite color");
+		System.out.println("If you do not know what ROYGBIV is, please type \"help\"");
+		String color = input.next();
+		if (color.equalsIgnoreCase("help")) {
+			System.out.println("The ROYGBIV colors are red, orange, yellow, green, blue, indigo, and violet.");
+			System.out.println("Now please enter your favorite of those colors.");
+			color = input.next();
+		}
 	}
 
 }
